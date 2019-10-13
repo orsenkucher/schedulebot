fa45cae60f43385568ede21a46fb9e46cc53bf84
Sergey Cheremshinsky <sergey.cheremshinsky@gmail.com>
2019-10-13 13:20:36 +0300

    Convert time to mins from sunday

--- cloudfunk/AddShedule.go
+++ cloudfunk/AddShedule.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 
 	"cloud.google.com/go/firestore"
 )
@@ -14,10 +15,14 @@ import (
 type Schedule struct {
 	Name        string   `firebase:"name" json:"name"`
 	Event       []string `firebase:"event" json:"event"`
-	Hour        []int    `firbase:"hour" json:"hour"`
 	Minute      []int    `firebase:"minute" json:"minute"`
-	Day         []string `firebase:"day" json:"day"`
 	Subscribers []string `firebase:"subscribers" json:"subscribers"`
+}
+
+func getMinsOfWeek() int {
+	t := time.Now()
+	mins := t.Minute() + t.Hour()*60 + int(t.Weekday())*24*60
+	return mins
 }
 
 // AddSchedule is cloud
